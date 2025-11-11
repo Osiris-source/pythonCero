4f22b5ba9cf758730d8a5e464f47322ca2cd0ce9
Osiris-source <flbecerrah@alumno.unsm.edu.pe>
2025-11-11 16:06:43 -0500

    subiendo mis sueños

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,1 +1,8 @@
-print("Hola Mundo")
+nombre = input("escribe tu nombre: ")
+profesion = input("escribe tu profesion: ")
+sueños = input("escribe tus sueños: ")
+
+print(
+    f"hola {nombre}, veo que eres {profesion} y que tus sueños son {sueños},"
+    " Espero de corazón que los cumplas pronto!"
+    )
